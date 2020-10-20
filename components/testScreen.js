7ce8de912f4780ff79e970941564abdb009822e7
ongsuwannoo <43009070+ongsuwannoo@users.noreply.github.com>
2020-10-20 21:55:52 +0700

    Update testScreen.js

--- components/testScreen.js
+++ components/testScreen.js
@@ -4,24 +4,30 @@ import {
   StyleSheet,
   TextInput,
   TouchableOpacity,
-  Alert 
+  Alert
 } from "react-native";
 import { Image, Text } from "react-native-elements";
 import externalStyle from "../style/externalStyle";
 import * as Animatable from "react-native-animatable";
 import axios from 'axios'
 
-
-
-
 class TestScreen extends Component {
-  
+
+  onLogout = () => {
+    console.log("Logout")
+  }
+
   render() {
     return (
       <View style={styles.container}>
-           <View style={{marginTop: 100, alignItems: 'center'}}>
-                <Text style={{color: 'black'}}>Test Screen</Text>
-            </View>
+        <View style={{ marginTop: 100, alignItems: 'center' }}>
+          <Text style={{ color: 'black', }}>Test Screen</Text>
+        </View>
+        <View style={styles.box_logout}>
+          <TouchableOpacity style={styles.logout_btn} onPress={() => { this.onLogout() }}>
+            <Text style={styles.text_button}>Logout</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
@@ -31,6 +37,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
+  },
+  text_button: {
+    fontSize: 16,
+    fontWeight: "400",
+    color: "#FFF",
+  },
+  logout_btn: {
+    width: "50%",
+    height: 40,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 3,
+    backgroundColor: "red",
+  },
+  box_logout: {
+    justifyContent: "flex-end",
+    alignItems: "center",
+    flex: 1,
+    marginBottom: 36
   }
 });
 
