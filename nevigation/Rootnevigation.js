7ec68b6ac22c885d23914b7864b9f2a1c0f05138
Sukrit leelakornkij <42915021+toeiisk@users.noreply.github.com>
2020-11-05 16:42:08 +0700

    upppppppppp

--- nevigation/Rootnevigation.js
+++ nevigation/Rootnevigation.js
@@ -3,13 +3,43 @@ import React from "react";
 import Signinscreen from "../components/LoginScreen";
 import Registerscreen from "../components/RegisterScreen";
 import LessonScreen from "../components/Lessonscreen";
+import { Image } from "react-native";
+import Color from "../assets/resources/constants/color";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const RootStack = createStackNavigator();
 
 const RootStackScreen = () => (
-  <RootStack.Navigator headerMode="none">
-    <RootStack.Screen name="SignInScreen" component={Signinscreen} />
-    <RootStack.Screen name="Register" component={Registerscreen} />
+  <RootStack.Navigator
+    screenOptions={{
+      headerStyle: { backgroundColor: Color.background_footer },
+    }}
+  >
+    <RootStack.Screen
+      name="SignInScreen"
+      component={Signinscreen}
+      options={{ headerShown: false }}
+    />
+    <RootStack.Screen
+      name="Register"
+      component={Registerscreen}
+      options={{
+        title: null,
+        headerLeft: () => (
+          <TouchableOpacity>
+            <Image
+              source={require("../assets/resources/icon/previous.png")}
+              style={{
+                width: 30,
+                height: 30,
+                marginLeft:30,
+                marginTop: 30
+              }}
+            />
+          </TouchableOpacity>
+        ),
+      }}
+    />
     {/* <RootStack.Screen name="Lessons" component={LessonScreen} /> */}
   </RootStack.Navigator>
 );
