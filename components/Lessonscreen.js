152fafe63dfc8197dbe799c6a0119d42ef83c727
Sukrit leelakornkij <42915021+toeiisk@users.noreply.github.com>
2020-11-10 02:24:09 +0700

    uppppppppppppppppppppp

--- components/Lessonscreen.js
+++ components/Lessonscreen.js
@@ -7,16 +7,60 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
-  ImageBackground,
+  TouchableHighlight,
   SafeAreaView,
   FlatList,
+  Modal,
   Image,
   ScrollView,
 } from "react-native";
 import * as Animatable from "react-native-animatable";
+import { Button, Overlay } from "react-native-elements";
+import { FloatingAction } from "react-native-floating-action";
 import Externalstyle from "../style/externalStyle";
+import Color from "../assets/resources/constants/color";
 class LessonScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      overlayVisible: false,
+      modalVisible: false,
+      nameselect: "",
+      nameselect2: "",
+      name: "",
+    };
+  }
+
+  setOverlayVisible = (visible, name) => {
+    this.setState({
+      overlayVisible: visible,
+      nameselect2: name,
+    });
+  };
+
+  setModalVisible = (visible, name) => {
+    this.setState({
+      modalVisible: visible,
+      nameselect: name,
+    });
+  };
   render() {
+    const { overlayVisible, nameselect2 } = this.state;
+    const { modalVisible, nameselect } = this.state;
+    const actions = [
+      {
+        text: "Create Classroom",
+        icon: require("../assets/logo.png"),
+        name: "CreateClass",
+        position: 2,
+      },
+      {
+        text: "Join Classroom",
+        icon: require("../assets/logo.png"),
+        name: "JoinClass",
+        position: 1,
+      },
+    ];
     return (
       <SafeAreaView style={Externalstyle.container}>
         <View style={Externalstyle.title_header}>
@@ -81,6 +125,44 @@ class LessonScreen extends Component {
             />
           </View>
         </ScrollView>
+        {nameselect2 == "CreateClass" ? (
+          <Overlay
+            onBackdropPress={() => {
+              this.setOverlayVisible(!overlayVisible);
+            }}
+            overlayStyle={{ backgroundColor: "transparent" }}
+          >
+            <View style={Externalstyle.classroom_centeredView}>
+              <Animatable.View
+                animation="zoomIn"
+                duration={2000}
+                style={Externalstyle.classroom_modalView}
+              ></Animatable.View>
+            </View>
+          </Overlay>
+        ) : nameselect2 == "JoinClass" ? (
+          <Overlay
+            onBackdropPress={() => {
+              this.setOverlayVisible(!overlayVisible);
+            }}
+            overlayStyle={{ backgroundColor: "transparent" }}
+          >
+            <View style={Externalstyle.classroom_centeredView}>
+              <Animatable.View
+                animation="zoomIn"
+                duration={2000}
+                style={Externalstyle.classroom_modalView}
+              ></Animatable.View>
+            </View>
+          </Overlay>
+        ) : null}
+        <FloatingAction
+          actions={actions}
+          color={Color.background_button_attendance}
+          onPressItem={(name) => {
+            this.setOverlayVisible(true, name);
+          }}
+        />
       </SafeAreaView>
     );
   }
